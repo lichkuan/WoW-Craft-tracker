c18436b098adc17a5f25fa6859562e6a5b495723
lichkuan <105985577+lichkuan@users.noreply.github.com>
2025-08-03 12:37:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Supprimé experimental.appDir qui est déprécié en Next.js 14
 }
 
 module.exports = nextConfig
