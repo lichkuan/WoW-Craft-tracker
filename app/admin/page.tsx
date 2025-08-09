9b93017d54fa319ebe073ee92a328910090476da
lichkuan <lichkuan@gmail.com>
2025-08-10 00:54:51 +0200

    chore(api): add debug ping/scan and fix enrich-all (node runtime)

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import CommunityEnrichPanel from "@/components/CommunityEnrichPanel";
+import CommunityEnrichPanel from "../../components/CommunityEnrichPanel";
 
 interface Character {
   shareId: string;
