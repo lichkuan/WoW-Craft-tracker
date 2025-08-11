d78dad22bdf3d023eaa08e2ff7671d3c4e55222a
lichkuan <lichkuan@gmail.com>
2025-08-11 15:13:58 +0200

    Maintenance

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,14 @@ export const metadata: Metadata = {
   description: 'Mists of Pandaria Classic',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>
-        <WowheadProvider />
-        {children}
+      <body>
+        <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "2rem" }}>
+          🚧 Site en maintenance vu que personne ne l'utilise 🚧
+        </div>
       </body>
     </html>
-  )
+  );
 }
